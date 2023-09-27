62dcf5cff5c9128cba61783d7e55a141053e703f
voito <voitovich200233@gmail.com>
2023-09-27 20:21:41 +0300

    cotextloads text disabled

--- src/test/java/io/voitovich/testmodsentask/modsentaskconfigserver/ModsenTaskConfigServerApplicationTests.java
+++ src/test/java/io/voitovich/testmodsentask/modsentaskconfigserver/ModsenTaskConfigServerApplicationTests.java
@@ -1,13 +1,14 @@
 package io.voitovich.testmodsentask.modsentaskconfigserver;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ModsenTaskConfigServerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Disabled
+    void contextLoads() {
+    }
 
 }
