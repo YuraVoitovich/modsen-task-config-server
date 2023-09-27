e2d8c91afbab793d3b9ac96304013fac0d956e7d
voito <voitovich200233@gmail.com>
2023-09-27 20:20:17 +0300

    Initial commit

---
+++ src/main/java/io/voitovich/testmodsentask/modsentaskconfigserver/ModsenTaskConfigServerApplication.java
@@ -0,0 +1,13 @@
+package io.voitovich.testmodsentask.modsentaskconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModsenTaskConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModsenTaskConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/voitovich/testmodsentask/modsentaskconfigserver/ModsenTaskConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.voitovich.testmodsentask.modsentaskconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModsenTaskConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
