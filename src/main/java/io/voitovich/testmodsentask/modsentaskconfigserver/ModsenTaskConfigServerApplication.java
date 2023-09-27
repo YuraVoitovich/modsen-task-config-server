123c18a6f597872476e54fefb258e091bda6316c
voito <voitovich200233@gmail.com>
2023-09-27 20:22:30 +0300

    Put EnableConfigServer annotation

--- src/main/java/io/voitovich/testmodsentask/modsentaskconfigserver/ModsenTaskConfigServerApplication.java
+++ src/main/java/io/voitovich/testmodsentask/modsentaskconfigserver/ModsenTaskConfigServerApplication.java
@@ -2,12 +2,14 @@ package io.voitovich.testmodsentask.modsentaskconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class ModsenTaskConfigServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ModsenTaskConfigServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ModsenTaskConfigServerApplication.class, args);
+    }
 
 }
